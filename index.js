ad376c5e912617cacde8b829d3f9e71c206fc9a8
imran-meno <menoimrankhan1@gmail.com>
2025-12-04 22:34:10 +0500

    added process exit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // index.js
-require("dotenv").config();
+require("dotenv").config({path:'.env'});
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
